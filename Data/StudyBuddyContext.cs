0a15a0e4bd7abe6c83fee2c52cc9d505f306927a
Aljaz <asmole33@gmail.com>
2026-01-08 16:43:56 +0100

    Removed Quiz and QuizQuestions tables

--- Data/StudyBuddyContext.cs
+++ Data/StudyBuddyContext.cs
@@ -12,8 +12,6 @@ namespace StudyBuddy.Data
         public DbSet<Subject> Subjects { get; set; }
         public DbSet<Topic> Topics { get; set; }
         public DbSet<Question> Questions { get; set; }
-        public DbSet<Quiz> Quizzes { get; set; }
-        public DbSet<QuizQuestion> QuizQuestions { get; set; }
         public DbSet<StudySession> StudySessions { get; set; }
         public DbSet<StudyTask> StudyTasks { get; set; }
 
@@ -25,27 +23,11 @@ namespace StudyBuddy.Data
                 .HasIndex(s => new { s.UserId, s.Name })
                 .IsUnique();
 
-            // Subject -> StudySession (you already used Restrict)
+            // Subject -> StudySession (Restrict)
             builder.Entity<StudySession>()
                 .HasOne(s => s.Subject)
                 .WithMany(su => su.StudySessions)
                 .HasForeignKey(s => s.SubjectId)
-                .OnDelete(DeleteBehavior.Restrict);
-
-            // QuizQuestion composite PK
-            builder.Entity<QuizQuestion>()
-                .HasKey(qq => new { qq.QuizId, qq.QuestionId });
-
-            builder.Entity<QuizQuestion>()
-                .HasOne(qq => qq.Quiz)
-                .WithMany(q => q.QuizQuestions)
-                .HasForeignKey(qq => qq.QuizId)
-                .OnDelete(DeleteBehavior.Restrict);
-
-            builder.Entity<QuizQuestion>()
-                .HasOne(qq => qq.Question)
-                .WithMany(q => q.QuizQuestions)
-                .HasForeignKey(qq => qq.QuestionId)
                 .OnDelete(DeleteBehavior.Restrict);
 
             // Subject -> StudyTask (Restrict)
@@ -61,14 +43,13 @@ namespace StudyBuddy.Data
                 .HasForeignKey(t => t.UserId)
                 .OnDelete(DeleteBehavior.Restrict);
 
-            // NEW: Subject -> Topics (CASCADE delete)
+            // Subject -> Topics (CASCADE delete)
             builder.Entity<Topic>()
                 .HasOne(t => t.Subject)
                 .WithMany(s => s.Topics)
                 .HasForeignKey(t => t.SubjectId)
                 .OnDelete(DeleteBehavior.Cascade);
 
-            // OPTIONAL BUT RECOMMENDED:
             // Unique topic names per subject (prevents duplicates like "Threads" twice)
             builder.Entity<Topic>()
                 .HasIndex(t => new { t.SubjectId, t.Name })
--- Migrations/20260108153732_RemoveQuizzes.Designer.cs
+++ Migrations/20260108153732_RemoveQuizzes.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using StudyBuddy.Data;
 
@@ -11,9 +12,11 @@ using StudyBuddy.Data;
 namespace StudyBuddy.Migrations
 {
     [DbContext(typeof(StudyBuddyContext))]
-    partial class StudyBuddyContextModelSnapshot : ModelSnapshot
+    [Migration("20260108153732_RemoveQuizzes")]
+    partial class RemoveQuizzes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
